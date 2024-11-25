9735902f9c8ad2797404dfc8eb6dd19821656158
George <giorgosmarkianos@gmail.com>
2024-11-25 19:29:28 +0200

    Final

--- Comms/wifi/server.py
+++ Comms/wifi/server.py
@@ -1,14 +1,26 @@
 import firebase_admin
 from firebase_admin import credentials, db
 
-cred = credentials.Certificate("/home/admin/Desktop/BAN/ban-net-firebase-adminsdk-xzaq7-e78cd98b2d.json")
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://ban-net-rtdb.europe-west1.firebasedatabase.app/',
-    'databaseAuthVariableOverride':{
-        'sercret': 'rasp_sensor'
-        }
-        
-    }) 
-
+cred = credentials.Certificate("/home/admin/Desktop/BAN/Comms/ban-net-firebase-adminsdk-xzaq7-e78cd98b2d.json")
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://ban-net-rtdb.europe-west1.firebasedatabase.app/'   
+    })
+    
+try:
+    ref = db.reference("/")
+    data = ref.get()
+    print("data",data)
+except Exception as e:
+    print("Error",e)
+    
 ref = db.reference('data')
-ref.set({'Heartrate': 50, 'O2:':99})
+
+try:
+    ref.set({
+        'Heartrate': 50,
+        'O2': 99
+    })
+    print("ok")
+except Exception as e:
+    print(f"Error writing data: {e}")

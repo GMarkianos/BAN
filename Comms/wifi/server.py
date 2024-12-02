1a6a1c5b1040ee584ecfdc03b1bc60d410a23583
George <giorgosmarkianos@gmail.com>
2024-12-02 12:17:06 +0200

    Bluetooth

--- Comms/wifi/server.py
+++ Comms/wifi/server.py
@@ -1,26 +1,15 @@
 import firebase_admin
 from firebase_admin import credentials, db
+import time
 
-cred = credentials.Certificate("/home/admin/Desktop/BAN/Comms/ban-net-firebase-adminsdk-xzaq7-e78cd98b2d.json")
+cred = credentials.Certificate("/home/admin/Desktop/ban-net-firebase-adminsdk-xzaq7-e78cd98b2d.json")
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred, {
-        'databaseURL': 'https://ban-net-rtdb.europe-west1.firebasedatabase.app/'   
+        "databaseURL": "https://ban-net-default-rtdb.europe-west1.firebasedatabase.app/"   
     })
     
-try:
-    ref = db.reference("/")
-    data = ref.get()
-    print("data",data)
-except Exception as e:
-    print("Error",e)
-    
-ref = db.reference('data')
+ref = db.reference("/")
 
-try:
-    ref.set({
-        'Heartrate': 50,
-        'O2': 99
-    })
-    print("ok")
-except Exception as e:
-    print(f"Error writing data: {e}")
+ref.update({"hr": 100})
+
+time.sleep(1)
